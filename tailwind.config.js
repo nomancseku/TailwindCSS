cde89afd67558622c597aa36eece9470f0ae0d3e
Abdullah Al Noman <41755275+nomancseku@users.noreply.github.com>
2024-09-19 12:40:04 +0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+    "./index.html"
+  ],
+  darkMode: 'selector',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
