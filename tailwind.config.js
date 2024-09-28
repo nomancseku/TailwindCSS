efcd13f3cbec1407cbd14685335723d778d2e6cb
Abdullah Al Noman <41755275+nomancseku@users.noreply.github.com>
2024-09-28 21:54:59 +0600

    L-14 ended (jit concept)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
+  mode: "jit", // it is enabled now by default, we dont need to write it anymore
+  content: ["./src/**/*.{html,js}", // content was previously known as purge!
     "./pages/**/*.{html,js}",
     "./components/**/*.{html,js}",
     "./index.html"
